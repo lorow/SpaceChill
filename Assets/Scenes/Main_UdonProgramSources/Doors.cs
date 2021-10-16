82f09e50024b8a8275d9315d7c9dd060bb6e8cb8
lorow <smykupyka@gmail.com>
2021-10-16 19:43:52 +0200

    improve layout and hierachy, add poc sliding doors

---
+++ Assets/Scenes/Main_UdonProgramSources/Doors.cs
@@ -0,0 +1,39 @@
+﻿
+using Thry;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class Doors : UdonSharpBehaviour
+{
+    public Collider playerTrigger;
+    public Transform leftDoor;
+    public Transform rightDoor;
+    public int doorOffset = 3;
+    public float slideTime = 5;
+    private float allowedOffset = 0.001f;
+
+    private void handleDoorPosition(Transform door, int slideVector = 1)
+    {
+        if (door == null)
+            return;
+
+        var doorPosition = door.position;
+        var desiredDoorPosition = new Vector3((doorPosition.x - (slideVector * allowedOffset)), doorPosition.y, doorPosition.z);
+        // TODO add actual slide mechanic   
+        
+    }
+    
+    public override void OnPlayerTriggerEnter(VRCPlayerApi player)
+    {
+        handleDoorPosition(leftDoor, -1);
+        handleDoorPosition(rightDoor);
+    }
+
+    public override void OnPlayerTriggerExit(VRCPlayerApi player)
+    {
+        handleDoorPosition(leftDoor, 1);
+        handleDoorPosition(rightDoor,-1);
+    }
+}
